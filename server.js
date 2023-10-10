f5c4dccdddaab77e81f5823b1173797de0f77cbb
Aluno Invest <aluno.invest@invest.local>
2023-10-09 22:08:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+const app = express()
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.set('view engine', 'ejs')
+app.get('/', function(request, response){
+response.render('pages/index')
+app.set('view engine', 'ejs')
+app.get('/', function(request, response){
+response.render('pages/about')
+app.set('view engine', 'ejs')
+app.get('/', function(request, response){
+response.render('pages/contato')
+})
+app.listen(8080)
+console.log('Server is running');
